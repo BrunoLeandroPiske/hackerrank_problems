b29313650f8ea58e9b9acfc857d0650ad7426536
Bruno Leandro Piske <leandropiskebruno@gmail.com>
2016-08-23 21:54:08 -0300

    Create file
    Create solution for problem number 6 in www.hackerrank.com

---
+++ 06SumSquareDifference.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    int N, i, N_number, e;
+    long total_square, total_sum, total, square;
+    scanf("%d", &N);
+    for (i = 0; i < N; i++) {
+        N_number = 0;
+        e = 0;
+        total_square = 0;
+        total_sum = 0;
+        total = 0;
+        square = 0;
+        scanf("%d", &N_number);
+        for (e = 1; e <= N_number; e++) {
+            total_square = pow(e, 2) + total_square;
+            total_sum = e + total_sum;
+        }
+        total = (pow(total_sum, 2) - total_square);
+        printf("%ld\n", total);
+    }
+    return 0;
+}
